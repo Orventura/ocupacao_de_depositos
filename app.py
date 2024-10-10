515fbedf1ba08744fa7c87f42fe81664baf6cfec
roberto <robertoventura.rvs@gmail.com>
2024-10-09 23:24:24 -0400

    Atualizadno o código do APP

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import duckdb as dk
 import pandas as pd
-from tkinter import filedialog
-#from PlanoArcon import *
 
-# Caminho para o arquivo Excel Finalizado
+
+
+# Caminho para o arquivo Excel Finalizado (alterar para comentário o bloco de  testes e descomentar esse bloco)
 #dcb003 = r"h:\EXPEDICAO\00 Painel de Controle\Programação\DCB003\DCB003.xlsx"
 #pdb988 = r"h:\EXPEDICAO\00 Painel de Controle\Programação\PDB988\PDB988.xlsx"
 #ftb101 = r"H:\EXPEDICAO\00 Painel de Controle\Programação\FTB101\FTB101.xlsx"
@@ -13,91 +13,54 @@ from tkinter import filedialog
 dcb003 = r"/media/roberto/500 gb/Projetos Python/Ocupação/Programação/DCB003/DCB003.xlsx"
 pdb988 = r"/media/roberto/500 gb/Projetos Python/Ocupação/Programação/PDB988/PDB988.xlsx"
 ftb101 = r"/media/roberto/500 gb/Projetos Python/Ocupação/Programação/FTB101/FTB101.xlsx"
-cubagem = "/media/roberto/500 gb/Projetos Python/Ocupação/18 Cubagens - Cabotagem  & Rodoviário/CUBAGEM.xlsx"
+cubagem = r"/media/roberto/500 gb/Projetos Python/Ocupação/18 Cubagens - Cabotagem  & Rodoviário/CUBAGEM.xlsx"
 
+# Carregar os arquivos Excel
 dfdcb003 = pd.read_excel(dcb003, decimal=",")
 dfpdb988 = pd.read_excel(pdb988, decimal=",")
 dfftb101 = pd.read_excel(ftb101, decimal=",")
 dfcubagem = pd.read_excel(cubagem, decimal=",")
 
-# Conecta ao DuckDB
+# Conectar ao DuckDB
 con = dk.connect()
 
-# Registra os DataFrames como tabelas temporárias
-# con.register('Plano_painel', df_painel)
-# con.register('Plano_interna', df_interna)
-# con.register('Plano_externa', df_externa)
+# Registrar os DataFrames no DuckDB
 con.register("dcb003", dfdcb003)
 con.register("pdb988", dfpdb988)
 con.register("ftb101", dfftb101)
 con.register("cubagem", dfcubagem)
 
-
-df_nfaturaveis_e_paletes = con.sql(
-    """
- SELECT
-    dcb003.item,
-    UPPER(dcb003.dep) AS Dep,
-    dcb003.quantidade,
-    UPPER(dcb003.loc) AS Loc,
-    cubagem.PALETIZAÇÃO,
-    dcb003.quantidade / cubagem.PALETIZAÇÃO AS paletes
-FROM
-    dcb003
-LEFT JOIN
-    cubagem
-ON
-    dcb003.item = cubagem.item
-
- WHERE
-    (dcb003.dep LIKE 'CQ' OR dcb003.dep LIKE 'REJ' OR dcb003.dep LIKE 'VCL' OR dcb003.dep LIKE 'DCL')
-    AND (dcb003.loc LIKE 'A2%')
-    """
-).df()
-
-df_nfaturaveis_e_paletes_final = con.sql(
-    """
+# Executar as consultas SQL para cada DataFrame
+df_nfaturaveis_e_paletes_final = con.sql("""
 WITH subconsulta AS
 (
-     SELECT
-     
+    SELECT
         dcb003.item,
-        
         UPPER(dcb003.dep) AS Dep,
         dcb003.quantidade,
         UPPER(dcb003.loc) AS Loc,
-        'Demais depósitos (Dev/REJ/CQ/FOR)' AS "Data",
-        
+        'Demais depósitos (DCL/REJ/VCL/CQ/FOR)' AS "Data",
         cubagem.PALETIZAÇÃO,
         dcb003.quantidade / cubagem.PALETIZAÇÃO AS paletes
     FROM
         dcb003
     LEFT JOIN
-        cubagem
-    ON
-        dcb003.item = cubagem.item
-
-     WHERE
+        cubagem ON dcb003.item = cubagem.item
+    WHERE
         (dcb003.dep LIKE 'CQ' OR dcb003.dep LIKE 'REJ' OR dcb003.dep LIKE 'VCL' OR dcb003.dep LIKE 'DCL' OR dcb003.dep LIKE 'FOR')
         AND (dcb003.loc LIKE 'A2%')
 )
-
 SELECT
-    
-    Data,
+    "Data",
     SUM(Quantidade) AS Produtos,
-    SUM(paletes) AS Paletes
+    CAST(SUM(Paletes) AS INT) AS Paletes
 FROM
     subconsulta
 GROUP BY
     "Data"
+""")
 
-    """
-
-).show()
-
-df_vaivem = con.sql(
-    """
+df_vaivem = con.sql("""
 WITH subquery AS
 (
     SELECT
@@ -110,27 +73,21 @@ WITH subquery AS
     FROM
         dcb003
     LEFT JOIN
-        cubagem
-    ON
-        dcb003.item = cubagem.item
-
-     WHERE
+        cubagem ON dcb003.item = cubagem.item
+    WHERE
         dcb003.loc LIKE 'TR-V%'
 )
-
 SELECT
     'Vai Vem' AS "Data",
     SUM(Qtd) AS Produtos,
-    SUM(Paletes) AS Paletes
+    CAST(SUM(Paletes) AS INT) AS Paletes
 FROM
     subquery
-GROUP BY 
+GROUP BY
     Loc
-    """
-).show()
+""")
 
-df_cif = con.sql(
-    """
+df_cif = con.sql("""
 WITH subquery AS
 (   
     SELECT
@@ -142,28 +99,22 @@ WITH subquery AS
     FROM
         ftb101
     INNER JOIN
-        cubagem
-    ON
-        ftb101.Item = cubagem.Item
+        cubagem ON ftb101.Item = cubagem.Item
     WHERE
         UPPER(ftb101.Depósito) IN ('ACA', 'EXP')
 )
-
 SELECT
     'CIF' AS "Data",
     SUM(Qtd) AS Produtos,
-    SUM(Paletes) AS Paletes
+    CAST(SUM(Paletes) AS INT) AS Paletes
 FROM
     subquery
 WHERE Frete LIKE 'CIF'
-
-GROUP BY 
+GROUP BY
     Frete
-"""
-).show()
+""")
 
-df_fob = con.sql(
-    """
+df_fob = con.sql("""
 WITH subquery AS
 (   
     SELECT
@@ -175,88 +126,163 @@ WITH subquery AS
     FROM
         ftb101
     INNER JOIN
-        cubagem
-    ON
-        ftb101.Item = cubagem.Item
+        cubagem ON ftb101.Item = cubagem.Item
     WHERE
         UPPER(ftb101.Depósito) IN ('ACA', 'EXP')
 )
-
 SELECT
     'FOB' AS "Data",
     SUM(Qtd) AS Produtos,
-    SUM(Paletes) AS Paletes
+    CAST(SUM(Paletes) AS INT) AS Paletes
 FROM
     subquery
 WHERE Frete LIKE 'FOB'
-
-GROUP BY 
+GROUP BY
     Frete
-"""
-).show()
+""")
 
-
-df_exp = con.sql(
-    """
+df_exp = con.sql("""
 WITH subquery AS
 (
     SELECT
-       dcb003.item,
-       UPPER(dcb003.dep) AS Dep,
-       dcb003.quantidade,
-       UPPER(dcb003.loc) AS Loc,
-       dcb003.quantidade / cubagem.PALETIZAÇÃO AS Paletes
+        dcb003.item,
+        UPPER(dcb003.dep) AS Dep,
+        dcb003.quantidade,
+        UPPER(dcb003.loc) AS Loc,
+        dcb003.quantidade / cubagem.PALETIZAÇÃO AS Paletes
     FROM
         dcb003
     LEFT JOIN
-        cubagem
-    ON
-        dcb003.item = cubagem.item
+        cubagem ON dcb003.item = cubagem.item
     WHERE
         dep LIKE 'EXP'
 )
 SELECT
     Dep,
     SUM(Quantidade) AS Produtos,
-    SUM(Paletes) AS Paletes
+    CAST(SUM(Paletes) AS INT) AS Paletes
 FROM
     subquery
 GROUP BY
     Dep
-   
-"""
-).show()
+""")
 
-#Sub consulta
-df_pl3_final = con.sql(
-    """
+df_pl3_final = con.sql("""
 WITH subconsulta AS
 (    
     SELECT
-       dcb003.item,
-       UPPER(dcb003.dep) AS Dep,
-       dcb003.quantidade,
-       UPPER(dcb003.loc) AS Loc,
-       dcb003.quantidade / cubagem.PALETIZAÇÃO AS Paletes
+        dcb003.item,
+        UPPER(dcb003.dep) AS Dep,
+        dcb003.quantidade,
+        UPPER(dcb003.loc) AS Loc,
+        dcb003.quantidade / cubagem.PALETIZAÇÃO AS Paletes
     FROM
         dcb003
     LEFT JOIN
-        cubagem
-    ON
-        dcb003.item = cubagem.item
+        cubagem ON dcb003.item = cubagem.item
     WHERE
         dep LIKE 'PL3' OR loc LIKE 'pl3'
 )
-
 SELECT
-    
     Dep,
-    SUM(Quantidade) AS Produtos,
-    SUM(Paletes) AS Paletes
+    CAST(SUM(Quantidade)AS INT) AS Produtos,
+    CAST(SUM(Paletes) AS INT) AS Paletes
 FROM
     subconsulta
 GROUP BY
     Dep
-   
-"""
-).show()
+""")
+
+
+df_exibir = con.sql("""
+WITH subquery AS (
+    SELECT * FROM df_nfaturaveis_e_paletes_final
+    UNION ALL
+    SELECT * FROM df_vaivem
+    UNION ALL
+    SELECT * FROM df_cif
+    UNION ALL
+    SELECT * FROM df_fob
+    UNION ALL
+    SELECT * FROM df_exp
+    UNION ALL
+    SELECT * FROM df_pl3_final
+)
+SELECT
+    "Data",
+    SUM(Produtos) AS Produtos,
+    SUM(Paletes) AS Paletes
+FROM
+    subquery
+GROUP BY
+    "Data"
+
+UNION ALL
+
+SELECT
+    'TOTAL' AS "Data",  -- Adiciona a linha de total
+    CAST(SUM(Produtos) AS INT) AS Produtos,  -- Total de Produtos
+    CAST(SUM(Paletes) AS INT) AS Paletes   -- Total de Paletes
+FROM
+    subquery
+""").show()
+
+df_final = con.sql("""
+WITH subquery AS (
+    SELECT * FROM df_nfaturaveis_e_paletes_final
+    UNION ALL
+    SELECT * FROM df_vaivem
+    UNION ALL
+    SELECT * FROM df_cif
+    UNION ALL
+    SELECT * FROM df_fob
+    UNION ALL
+    SELECT * FROM df_exp
+    UNION ALL
+    SELECT * FROM df_pl3_final
+)
+SELECT
+    "Data",
+    SUM(Produtos) AS Produtos,
+    SUM(Paletes) AS Paletes
+FROM
+    subquery
+GROUP BY
+    "Data"
+
+UNION ALL
+
+SELECT
+    'TOTAL' AS "Data",  -- Adiciona a linha de total
+    CAST(SUM(Produtos) AS INT) AS Produtos,  -- Total de Produtos
+    CAST(SUM(Paletes) AS INT) AS Paletes   -- Total de Paletes
+FROM
+    subquery
+""").df()
+
+
+
+legenda = {
+    'Categoria': ['EXP', 'CIF', 'FOB', 'Ñ FATURÁVEL', 'VAI-VEM', 'PL3'],
+    'Descrição': [
+        'PA não Faturado',
+        'Faturado não embarcado CIF',
+        'Faturado não embarcado FOB',
+        'Demais depósitos (DCL/REJ/VCL/CQ/FOR)',
+        'PRODUTOS EM TRANPORTE',
+        'DESCASADOS EM PL3'
+    ]
+}
+
+
+
+df_legenda = pd.DataFrame(legenda)
+
+html1 = df_final.to_html(index=False, decimal=',')
+html2 = df_legenda.to_html(index=False, decimal=',')
+
+def tabela1():
+    return html1
+
+def tabela2():
+    return html2
---
+++ teste.py
@@ -0,0 +1,4 @@
+import duckdb as dk
+import pandas as pd
+from app import html1, html2
+
